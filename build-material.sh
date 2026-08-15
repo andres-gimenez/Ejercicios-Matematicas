34110c1225c45a95666e2fb096811fcda2c2031d
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-08-15 18:40:26 +0200

    Filtar Apuntes

--- build-material.sh
+++ build-material.sh
@@ -1,4 +1,11 @@
-find ./Material -type f -name "*.typ" | while read -r archivo; do
+find Material -type f -name "*.typ" -print |
+while IFS= read -r archivo; do
+    # Si está dentro de Apuntes, comprobar que está directamente dentro
+    if [[ "$archivo" == **/Apuntes/** ]]; then
+        continue
+    fi
+
     echo "Compilando: $archivo"
-    typst compile "$archivo" --root . --input show-solutions=true || echo "Error en $archivo, continuando..."
+    typst compile "$archivo" --root . --input show-solutions=true ||
+        echo "Error en $archivo, continuando..."
 done
